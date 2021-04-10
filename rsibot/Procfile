9be730e7f83aff5ef47c3f63b4460ab4b48fc31f
Igor Iglesias <contato@igoriglesias.com>
2021-04-10 08:52:18 -0300

    Check minimum gain before sell

--- rsibot/Procfile
+++
@@ -1,1 +0,0 @@
-worker: python bot.py
--- rsibot/bot.py
+++ rsibot/bot.py
@@ -5,32 +5,37 @@ from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethbrl@kline_1m"
 
-RSI_PERIOD = 7
-RSI_OVERBOUGHT = 60
-RSI_OVERSOLD = 40
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'ETHBRL'
 TRADE_QUANTITY = 0.05
+MINIMUM_GAIN = 0.30
 
 closes = []
 in_position = False
 
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET, last_rsi=0):
+def order(side, quantity, symbol, last_price, gain=0 , order_type=ORDER_TYPE_MARKET):
     try:
-        print("sending order")
+        # print("sending order")
         # order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        f = open("negociacoes.txt", mode="a+", newline="\r\n")
-        f.write(str(order_type) + " " + str(quantity) + " " + str(symbol) + " " + str(side) + " " + str(last_rsi) )
+        f = open("negociacoes.txt", mode="a+", newline="\n")
+        f.write("Operação: " + str(side) + " Quantidade: " + str(quantity) + " Valor: " + str(last_price)+ " Ganho: " + str(gain) + "\n" )
         f.close()
-        print(order)
+        # print(order)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
 
     return True
 
-    
+def gain(sold, bought):
+  diff = float(sold) - float(bought)
+  gain = (diff / bought) * 100
+  return gain
+
 def on_open(ws):
     print('opened connection')
 
@@ -39,7 +44,7 @@ def on_close(ws):
 
 def on_message(ws, message):
     global closes, in_position
-    
+    bought = 0
     # print('received message')
     json_message = json.loads(message)
     # pprint.pprint(json_message)
@@ -65,11 +70,16 @@ def on_message(ws, message):
             
             if last_rsi > RSI_OVERBOUGHT:
                 if in_position:
-                    print("Overbought! Sell! Sell! Sell!")
-                    # put binance sell logic here
-                    order_succeeded = order("SELL", TRADE_QUANTITY, TRADE_SYMBOL, last_rsi)
-                    if order_succeeded:
-                        in_position = False
+                    sold = close
+                    gain = gain(sold, bought)
+                    if gain >= MINIMUM_GAIN:
+                      print("Overbought! Sell! Sell! Sell!")
+                      # put binance sell logic here
+                      order_succeeded = order("SELL", TRADE_QUANTITY, TRADE_SYMBOL, close, gain)
+                      if order_succeeded:
+                          in_position = False
+                    else:
+                      print("Overbought! But not profitable, nothing to do.")
                 else:
                     print("It is overbought, but we don't own any. Nothing to do.")
             
@@ -77,9 +87,11 @@ def on_message(ws, message):
                 if in_position:
                     print("It is oversold, but you already own it, nothing to do.")
                 else:
+                    bought = float(last_price)
                     print("Oversold! Buy! Buy! Buy!")
                     # put binance buy order logic here
-                    order_succeeded = order("BUY", TRADE_QUANTITY, TRADE_SYMBOL, last_rsi)
+                    order_succeeded = order("BUY", TRADE_QUANTITY, TRADE_SYMBOL, last_price)
+                    
                     if order_succeeded:
                         in_position = True
 
